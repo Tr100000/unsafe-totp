a80bb4e5a02da0ec507d4a76a956b46f2ca487c6
Tr100000 <tomnramler@gmail.com>
2023-12-17 11:39:08 +0900

    dont save to local storage

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,6 @@ import * as Auth from "otpauth";
 const secretInput = document.getElementById("secret") as HTMLInputElement;
 const tokenText = document.getElementById("token") as HTMLParagraphElement;
 
-secretInput.value = window.localStorage.getItem("secret") ?? "";
-
 const totp = new Auth.TOTP({
     algorithm: "SHA1",
     digits: 6,
@@ -14,7 +12,6 @@ const totp = new Auth.TOTP({
 function generateToken() {
     totp.secret = Auth.Secret.fromBase32(secretInput.value);
     tokenText.innerHTML = totp.generate();
-    window.localStorage.setItem("secret", secretInput.value);
 }
 
 setInterval(generateToken, 500);
